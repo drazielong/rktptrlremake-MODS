2457be8769dc63ce64f53d8eb6e97940471b999c
Rosie <rclongo@ucsc.edu>
2021-04-07 14:07:20 -0700

    menu test

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+class menu extends Phaser.Scene {
+    constructor (){
+        super("menuScene");
+    }
+
+    create() {
+        this.add.text(20,20, "Rocket Patrol Menu"); //create text on this scene at 20,20
+    }
+}

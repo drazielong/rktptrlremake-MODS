e80986401d12bd05508eeb979302c3b969adb3c0
Rosie <rclongo@ucsc.edu>
2021-04-07 14:19:58 -0700

    green rectangle on screen

---
+++ src/Scenes/play.js
@@ -0,0 +1,12 @@
+class play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    create() {
+        //green ui bg - add and rectangle come from phaser framework
+        //parameters: ?, ?, width, height, color
+        //Im guessing the first two parameters are the start corner as in where to draw the rectangle
+        this.add.rectangle(0, borderUISize + BorderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0); //default origin is the middle of rectangle
+    }
+}
--- src/main.js
+++ src/main.js
@@ -3,7 +3,11 @@ console.log("heyyy")
 let config = {
     type: Phaser.CANVAS,
     width: 640,
-    height: 480
+    height: 480,
+    scene: [menu, play]
 };
 
 let game = new Phaser.Game(config);
+
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;

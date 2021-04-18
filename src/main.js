fe1d95866bb01d63149ea436b94a7d49bbabd979
Rosie <rclongo@ucsc.edu>
2021-04-18 02:30:23 -0700

    Added name and date etc.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+//Rosie Longo
+//Popsicle Patrol
+//April 18, 2021
+//3 hrs so far
+//Reskinned the game to be popsicle shooting game and not space (60 points)
+//added background music to menu and main game (5 points)
+//changed the background to have an animated sprite
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
@@ -10,5 +18,4 @@ let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 
-//reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
